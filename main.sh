2b5f2ad749314f2700093722107a38c7a96d1e69
kherbik <kamil.herbik@rst.com.pl>
2017-07-28 15:18:17 +0200

    [get_params] - added new function which searches for all of the key/value - removed duplicated functions
    Changes to be committed:
    	modified:   main.sh

--- main.sh
+++ main.sh
@@ -6,9 +6,11 @@
 #
 set -e
 
+readonly AWK='/bin/awk'
 readonly BASE64='/bin/base64'
 readonly CURL='/bin/curl'
 readonly GETOPTS='/bin/getopts'
+readonly GREP='/bin/grep'
 readonly JQ='/bin/jq'
 readonly RPM='/bin/rpm'
 readonly TR='/bin/tr'
@@ -19,8 +21,20 @@ APPLICATION=""
 CONSUL_ADDR=""
 TEST=false
 
+function _check_key {
+	local CONSUL_OUTPUT=""
+
+	CONSUL_OUTPUT=$(${CURL} -s -XGET http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}?recurse=true)
+  if [ -z "${CONSUL_OUTPUT}" ]; then
+		echo "Your '-a APPLICAITON' is wrong, this key does not exist at Consul!" >&2
+		exit 1
+	fi
+}
+
 function _get_version {
 	local VERSION=""
+
+  _check_key
 
 	VERSION=$(${CURL} -s http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}/versions/stable | ${JQ} '.[0].Value // empty' | ${TR} -d '\"' | ${BASE64} --decode)
   if [ -z "${VERSION}" ]; then
@@ -31,70 +45,20 @@ function _get_version {
 	fi
 }
 
-function _get_application_env {
-	local APPLICATION_ENV=""
+function _get_variables {
 	local VERSION=""
 
-	VERSION=$(_get_version)
-
-	APPLICATION_ENV=$(${CURL} -s http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}/configs/${VERSION}/APPLICATION_ENV | ${JQ} '.[0].Value // empty' | ${TR} -d '\"' | ${BASE64} --decode)
-  if [ -z "${APPLICATION_ENV}" ]; then
-		echo "The key is empty, cannot determine APPLICATION_ENV of the ${APPLICATION}-${VERSION}!" >&2
-		exit 1
-	else
-		echo "${APPLICATION_ENV}"
-	fi
-}
-
-function _get_exec_user {
-	local EXEC_USER=""
-	local VERSION=""
+  _check_key
 
 	VERSION=$(_get_version)
-
-	EXEC_USER=$(${CURL} -s http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}/configs/${VERSION}/EXEC_USER | ${JQ} '.[0].Value // empty' | ${TR} -d '\"' | ${BASE64} --decode)
-  if [ -z "${EXEC_USER}" ]; then
-		echo "The key is empty, cannot determine EXEC_USER of the ${APPLICATION}-${VERSION}!" >&2
-		exit 1
-	else
-		echo "${EXEC_USER}"
-	fi
-}
-
-function _get_exec_group {
-	local EXEC_GROUP=""
-	local VERSION=""
-
-	VERSION=$(_get_version)
-
-	EXEC_GROUP=$(${CURL} -s http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}/configs/${VERSION}/EXEC_GROUP | ${JQ} '.[0].Value // empty' | ${TR} -d '\"' | ${BASE64} --decode)
-  if [ -z "${EXEC_GROUP}" ]; then
-		echo "The key is empty, cannot determine EXEC_GROUP of the ${APPLICATION}-${VERSION}!" >&2
-		exit 1
-	else
-		echo "${EXEC_GROUP}"
-	fi
-}
-
-function _get_application_root {
-	local APPLICATION_ROOT=""
-	local VERSION=""
-
-	VERSION=$(_get_version)
-
-	APPLICATION_ROOT=$(${CURL} -s http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}/configs/${VERSION}/APPLICATION_ROOT | ${JQ} '.[0].Value // empty' | ${TR} -d '\"' | ${BASE64} --decode)
-  if [ -z "${APPLICATION_ROOT}" ]; then
-		echo "The key is empty, cannot determine APPLICATION_ROOT of the ${APPLICATION}-${VERSION}!" >&2
-		exit 1
-	else
-		echo "${APPLICATION_ROOT}"
-	fi
+	PARAMS=$(${CURL} -s http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}/configs/${VERSION}?recurse=true | ${JQ} -r '.[] | .Key + " " + .Value' | ${AWK} -F'/' '{print $NF}' | ${GREP} $1 | ${AWK} -F ' ' '{print $2}' | ${BASE64} --decode)
+	echo "${PARAMS}"
 }
 
 function _test_if_installed {
 #
-# if -t then only TEST if application installed or not
-# if -t not set then perform install action
+# if -t provided then only TEST if application installed or not
+# if -t provided not set then perform install action
 #
 	local INSTALL_CHECK=""
 	local VERSION=""
@@ -130,9 +94,9 @@ function parse_opts {
 	    c)
 	      CONSUL_ADDR="${OPTARG}"
 	      ;;
-		  t)
-			  TEST=true
-				;;
+      t)
+        TEST=true
+        ;;
 	    \?)
 	      echo "Invalid option: -${OPTARG}" >&2
 	      exit 1
@@ -154,16 +118,6 @@ function parse_opts {
 
 }
 
-function check_key {
-	local CONSUL_OUTPUT=""
-
-	CONSUL_OUTPUT=$(${CURL} -s -XGET http://${CONSUL_ADDR}:8500/v1/kv/services/${APPLICATION}?recurse=true)
-  if [ -z "${CONSUL_OUTPUT}" ]; then
-		echo "Your '-a APPLICAITON' is wrong, this key does not exist at Consul!" >&2
-		exit 1
-	fi
-}
-
 function install_application {
 	local APPLICATION_ENV=""
 	local APPLICATION_ROOT=""
@@ -172,12 +126,13 @@ function install_application {
 	local INSTALL_CHECK=""
 	local VERSION=""
 
-	APPLICATION_ROOT=$(_get_application_root)
-	EXEC_GROUP=$(_get_exec_group)
-	EXEC_USER=$(_get_exec_user)
+	APPLICATION_ENV=$(_get_variables APPLICATION_ENV)
+	APPLICATION_ROOT=$(_get_variables APPLICATION_ROOT)
+	EXEC_GROUP=$(_get_variables EXEC_GROUP)
+	EXEC_USER=$(_get_variables EXEC_USER)
+
 	INSTALL_CHECK=$(_test_if_installed)
 	VERSION=$(_get_version)
-  APPLICATION_ENV=$(_get_application_env)
 
   if [ "${TEST}" = true ]; then
 		if [ "${INSTALL_CHECK}" -eq 0 ]; then
@@ -186,8 +141,6 @@ function install_application {
 			exit 1
 		fi
 	else
-		printf "Checking wether an application %s is installed\n" "${APPLICATION}-${VERSION}" >&2
-
 		if [ "${INSTALL_CHECK}" -eq 1 ]; then
 			printf "Installing an application: %s\n" "${APPLICATION}-${VERSION}\n" >&2
 			printf "APPLICATION_ENV=%s APPLICATION_ROOT=%s EXEC_USER=%s EXEC_GROUP=%s" "${APPLICATION_ENV}" "${APPLICATION_ROOT}" "${EXEC_USER}" "${EXEC_GROUP}" >&2
@@ -206,7 +159,6 @@ function install_application {
 
 function main {
 	parse_opts "${@}"
-	check_key
 	install_application
 	exit 0
 }
